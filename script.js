4dbcd5d8b6942e30ba6bc5927b5cb092868e492a
Anthony Lapham <lapham.2@wright.edu>
2017-08-06 12:26:59 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+  // Will we have to round numbers for precision?
+  var defaultWorkDurationMinute = 25;
+  var defaultWorkDurationSecond = 0;
+  var defaultBreakDurationMinute = 05;
+  var defaultBreakDurationSecond = 0;
+
+  var workMinute = defaultWorkDurationMinute.toString();
+  var workSecond = defaultWorkDurationSecond ? defaultWorkDurationSecond.toString() : '00';
+  var breakMinute = defaultBreakDurationSecond.toString();
+  var breakSecond = defaultBreakDurationSecond ? defaultBreakDurationSecond.toString() : '00';
+
+  $('#start-work').on('click', function() {
+    $('#clock').text(workMinute + ':' + workSecond)
+    var workTimerId = setInterval(function() {
+      if(defaultWorkDurationSecond === 0) {
+        defaultWorkDurationSecond = 59;
+        defaultWorkDurationMinute -= 1;
+      }
+      else {
+        defaultWorkDurationSecond -= 1;
+      }
+      workMinute = defaultWorkDurationMinute.toString()
+      workSecond = defaultWorkDurationSecond ? defaultWorkDurationSecond.toString() : '00'
+      $('#clock').text(workMinute + ':' + workSecond)
+
+      if(!defaultWorkDurationMinute && !defaultWorkDurationSecond) {
+        defaultWorkDurationMinute = 25;
+        defaultWorkDurationSecond = 0;
+        clearInterval(workTimerId);
+      }
+    }, 1000)
+  });
+
+  $('#start-break').on('click', function() {
+    $('#clock').text(breakMinute + ':' + breakSecond)
+    var breakTimerId = setInterval(function() {
+      if(defaultBreakDurationSecond === 0){
+        defaultBreakDurationSecond = 59;
+        defaultBreakDurationMinute -= 1;
+      }
+      else{
+        defaultBreakDurationSecond -= 1;
+      }
+      breakMinute = defaultBreakDurationSecond.toString();
+      breakSecond = defaultBreakDurationSecond ? defaultBreakDurationSecond.toString() : '00';
+      $('#clock').text(breakMinute + ':' + breakSecond)
+
+      if(!defaultBreakDurationMinute && !defaultBreakDurationSecond){
+        defaultBreakDurationMinute = 05;
+        defaultBreakDurationSecond = 0;
+        clearInterval(breakTimerId);
+      }
+    }, 1000);
+  })
+});
