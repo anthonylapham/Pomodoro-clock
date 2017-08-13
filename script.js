f1c25f9f6e82f48f906d63bcc79aaf4777bac50e
Anthony Lapham <lapham.2@wright.edu>
2017-08-13 12:17:41 -0400

    new events

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+  var breakTimerId = null;
+  var workTimerId = null;
+
   var defaultWorkDurationMinute = 25;
   var defaultWorkDurationSecond = 0;
   var defaultBreakDurationMinute = 5;
@@ -7,12 +10,71 @@ $(document).ready(function(){
 
   var workMinute = defaultWorkDurationMinute.toString();
   var workSecond = defaultWorkDurationSecond ? defaultWorkDurationSecond.toString() : '00';
-  var breakMinute = defaultBreakDurationSecond.toString();
+  var breakMinute = defaultBreakDurationMinute.toString();
   var breakSecond = defaultBreakDurationSecond ? defaultBreakDurationSecond.toString() : '00';
 
+  $('#reset').hide()
+
+  function hideButtons(override = false){
+    override ? $('#start-break').show() : $('#start-break').hide();
+    override ? $('#start-work').show() : $('#start-work').hide();
+    override ? $('#increase-session').show() : $('#increase-session').hide();
+    override ? $('#decrease-session').show() : $('#decrease-session').hide();
+    override ? $('#increase-break').show() : $('#increase-break').hide();
+    override ? $('#decrease-break').show() : $('#decrease-break').hide();
+    override ? $('#reset').hide() : $('#reset').show();
+  }
+
+  $('#increase-session').on('click', function(){
+    defaultWorkDurationMinute++
+    workMinute = defaultWorkDurationMinute.toString();
+
+    var increaseMinute = $('#startMinute').text()
+    increaseMinute = parseInt(increaseMinute, 10);
+    increaseMinute++
+    increaseMinute.toString()
+    $('#startMinute').text(increaseMinute);
+  });
+
+  $('#decrease-session').on('click', function(){
+    defaultWorkDurationMinute--
+    workMinute = defaultWorkDurationMinute.toString();
+
+    var decreaseMinute = $('#startMinute').text()
+    decreaseMinute = parseInt(decreaseMinute, 10);
+    decreaseMinute--
+    decreaseMinute.toString();
+    $('#startMinute').text(decreaseMinute);
+  });
+
+  $('#increase-break').on('click', function(){
+    defaultBreakDurationMinute++
+    breakMinute = defaultBreakDurationMinute.toString();
+
+    var increaseBreak = $('#startBreak').text();
+    increaseBreak = parseInt(increaseBreak, 10);
+    increaseBreak++
+    increaseBreak.toString();
+    $('#startBreak').text(increaseBreak);
+  });
+
+  $('#decrease-break').on('click', function(){
+    defaultBreakDurationMinute--
+    breakMinute = defaultBreakDurationMinute.toString();
+
+    var decreaseBreak = $('#startBreak').text();
+    decreaseBreak = parseInt(decreaseBreak, 10);
+    decreaseBreak--
+    decreaseBreak.toString();
+    $('#startBreak').text(decreaseBreak);
+  });
+
   $('#start-work').on('click', function() {
+    hideButtons();
+
     $('#clock').text(workMinute + ':' + workSecond)
-    var workTimerId = setInterval(function() {
+
+    workTimerId = setInterval(function() {
       if(defaultWorkDurationSecond === 0) {
         defaultWorkDurationSecond = 59;
         defaultWorkDurationMinute -= 1;
@@ -33,8 +95,11 @@ $(document).ready(function(){
   });
 
   $('#start-break').on('click', function() {
+    hideButtons();
+
     $('#clock').text(breakMinute + ':' + breakSecond)
-    var breakTimerId = setInterval(function() {
+
+    breakTimerId = setInterval(function() {
       if(defaultBreakDurationSecond === 0){
         defaultBreakDurationSecond = 59;
         defaultBreakDurationMinute -= 1;
@@ -42,7 +107,7 @@ $(document).ready(function(){
       else{
         defaultBreakDurationSecond -= 1;
       }
-      breakMinute = defaultBreakDurationSecond.toString();
+      breakMinute = defaultBreakDurationMinute.toString();
       breakSecond = defaultBreakDurationSecond ? defaultBreakDurationSecond.toString() : '00';
       $('#clock').text(breakMinute + ':' + breakSecond)
 
@@ -54,7 +119,20 @@ $(document).ready(function(){
     }, 1000);
   });
 
-  $('#reset').on('click', function(){
+  $('#reset').on('click', function(e){
+    hideButtons(true);
     $('#clock').text('0:00');
+    clearInterval(workTimerId);
+    clearInterval(breakTimerId);
+    defaultWorkDurationMinute = 25;
+    defaultWorkDurationSecond = 0;
+    defaultBreakDurationMinute = 5;
+    defaultBreakDurationSecond = 0;
+    workMinute = defaultWorkDurationMinute.toString();
+    workSecond = defaultWorkDurationSecond ? defaultWorkDurationSecond.toString() : '00';
+    breakMinute = defaultBreakDurationMinute.toString();
+    breakSecond = defaultBreakDurationSecond ? defaultBreakDurationSecond.toString() : '00';
+
+
   });
 });
